0fff8af1049e6cf375c4bf79ca43b4281e22133d
Matthew Harvey <software@matthewharvey.net>
2016-06-24 18:49:38 +0930

    Improve code organisation.

--- angry-loader.js
+++ angry-loader.js
@@ -25,7 +25,7 @@ var AngryLoader = function($) {
   };
 
 
-  // initialization
+  // core logic
 
   function initialize(options) {
     $.extend(opts, options);
@@ -37,27 +37,15 @@ var AngryLoader = function($) {
     }
   }
 
-  function contentBetween(str, bookends) {
-      var startAt = bookends[0];
-      var endAt = bookends[1];
-      var startPos = (startAt ? str.search(startAt) + startAt.length : 0);
-      var endPos = (endAt ? str.search(endAt) : undefined);
-      return str.slice(startPos, endPos);
-  }
-
-  function handleBack(/*event*/) {
-    var url = document.location.pathname;
-    if (url in cache) {
-      load(url);
-    }
-  }
-
-  function notifyLoaded() {
-    doc.trigger('angryLoader:load');
-  }
-
-  function historyAPISupported() {
-    return (typeof window.history !== 'undefined' && typeof window.history.pushState === 'function');
+  function initializeLinks(elems) {
+    $('a').not('.js-angry-loader-initialized').click(function(event) {
+      var url = $(this).attr('href');
+      if (url in cache) {
+        event.preventDefault();
+        window.history.pushState({}, undefined, url);
+        load(url);
+      }
+    }).addClass('js-angry-loader-initialized');
   }
 
   function populateCache() {
@@ -73,17 +61,6 @@ var AngryLoader = function($) {
     });
   }
 
-  function initializeLinks(elems) {
-    $('a').not('.js-angry-loader-initialized').click(function(event) {
-      var url = $(this).attr('href');
-      if (url in cache) {
-        event.preventDefault();
-        window.history.pushState({}, undefined, url);
-        load(url);
-      }
-    }).addClass('js-angry-loader-initialized');
-  }
-
   function load(url) {
     var cached = cache[url];
     doc.find('title').html(cached.title);
@@ -92,7 +69,39 @@ var AngryLoader = function($) {
     notifyLoaded();
   }
 
-  return {  // export public functions
+  function notifyLoaded() {
+    doc.trigger('angryLoader:load');
+  }
+
+  function handleBack(/*event*/) {
+    var url = document.location.pathname;
+    if (url in cache) {
+      load(url);
+    }
+  }
+
+
+  // string utilities
+
+  function contentBetween(str, bookends) {
+      var startAt = bookends[0];
+      var endAt = bookends[1];
+      var startPos = (startAt ? str.search(startAt) + startAt.length : 0);
+      var endPos = (endAt ? str.search(endAt) : undefined);
+      return str.slice(startPos, endPos);
+  }
+
+
+  // feature detection
+
+  function historyAPISupported() {
+    return (typeof window.history !== 'undefined' && typeof window.history.pushState === 'function');
+  }
+
+
+  // export public functions
+
+  return {
     initializeLinks: initializeLinks,
     initialize:     initialize
   };
